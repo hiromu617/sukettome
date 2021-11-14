7825151f3fd13c3466afe461265366f073045bde
hiro <hiromhiro617@gmail.com>
2021-11-14 22:28:45 +0900

    :sparkles: add Drawer

---
+++ src/components/Layout/DrawerNav.tsx
@@ -0,0 +1,39 @@
+import React, { VFC } from 'react';
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react';
+
+type DrawerNavProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export const DrawerNav: VFC<DrawerNavProps> = ({isOpen, onClose}) => {
+  return (
+    <>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody></DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,34 +1,40 @@
 import React, { VFC } from 'react';
-import { Container, VStack, Box, Flex, Button, IconButton, Spacer } from '@chakra-ui/react';
+import { Box, Flex, Button, IconButton, Spacer, useDisclosure } from '@chakra-ui/react';
 import { Image } from '@chakra-ui/image';
 import Link from 'next/link';
 import { HamburgerIcon } from '@chakra-ui/icons';
+import { DrawerNav } from './DrawerNav';
 
 export const Header: VFC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box h="60px" bg="white" shadow="md" zIndex="banner" pos="sticky" top="0" w="100%">
-      <Flex w="100%" h="100%" alignItems="center" justify="space-between" p="4">
-        <IconButton
-          variant="goast"
-          mr={[0, 2]}
-          color="gray.900"
-          aria-label="Call Segun"
-          size="lg"
-          icon={<HamburgerIcon w="6" h="6" />}
-        />
-        <Link href="/" passHref>
-          <Image src={'logo.png'} w="auto" h="24px" alt="logo" mb="1" />
-        </Link>
-        <Spacer display={['none', 'block']} />
-        <Button
-          color="white"
-          bg="gray.900"
-          _hover={{ bg: 'gray.500' }}
-          onClick={() => alert('login')}
-        >
-          Login
-        </Button>
-      </Flex>
-    </Box>
+    <>
+      <Box h="60px" bg="white" shadow="md" zIndex="banner" pos="sticky" top="0" w="100%">
+        <Flex w="100%" h="100%" alignItems="center" justify="space-between" p="4">
+          <IconButton
+            variant="goast"
+            mr={[0, 2]}
+            color="gray.900"
+            aria-label="Call Segun"
+            size="lg"
+            icon={<HamburgerIcon w="6" h="6" />}
+            onClick={onOpen}
+          />
+          <Link href="/" passHref>
+            <Image src={'logo.png'} w="auto" h="24px" alt="logo" mb="1" />
+          </Link>
+          <Spacer display={['none', 'block']} />
+          <Button
+            color="white"
+            bg="gray.900"
+            _hover={{ bg: 'gray.500' }}
+            onClick={() => alert('login')}
+          >
+            Login
+          </Button>
+        </Flex>
+      </Box>
+      <DrawerNav isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
