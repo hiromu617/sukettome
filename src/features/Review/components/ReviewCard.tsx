4d69de9038abab688b7870772c1862448c333a8f
hiro <hiromhiro617@gmail.com>
2021-12-12 16:32:02 +0900

    :sparkles: add date format

--- src/features/Review/components/ReviewCard.tsx
+++ src/features/Review/components/ReviewCard.tsx
@@ -1,10 +1,11 @@
 import { VFC } from 'react';
-import { Box, Text, Flex, Stack, Avatar } from '@chakra-ui/react';
+import { Box, Text, Flex, Stack, Avatar, Spacer } from '@chakra-ui/react';
 import type { Review } from '../';
 import Link from 'next/link';
 import { SkatingHistoryConst } from '../../User/index';
 import { StarIcon } from '@chakra-ui/icons';
 import Rating from 'react-rating';
+import { format } from 'date-fns';
 
 type ReviewCardProps = {
   review: Review;
@@ -16,7 +17,7 @@ export const ReviewCard: VFC<ReviewCardProps> = ({ review }) => {
       <Link href={`/user/${review.users.id}`} passHref>
         <Avatar name={review.users.user_name} src={review.users.avatar_url} />
       </Link>
-      <Stack px={2}>
+      <Stack px={2} flex="1">
         <Flex align="center">
           <Link href={`/user/${review.users.id}`} passHref>
             <Text fontSize={'lg'} color="gray.600" fontWeight="bold">
@@ -39,6 +40,10 @@ export const ReviewCard: VFC<ReviewCardProps> = ({ review }) => {
                 ? SkatingHistoryConst[review.users.skating_history]
                 : '未設定'}
             </span>
+          </Text>
+          <Spacer />
+          <Text color="gray.600" fontSize="sm" ml="3">
+            {format(new Date(review.created_at), 'yyyy/MM/dd HH:mm')}
           </Text>
         </Flex>
         <Rating
