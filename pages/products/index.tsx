bdf314c2c32cfddf0c34807abb5698e30b2f1220
hiro <hiromhiro617@gmail.com>
2021-12-25 22:47:15 +0900

    :sparkles: impl fetch

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,32 +1,48 @@
 import type { NextPage, GetServerSideProps } from 'next';
 import { VStack } from '@chakra-ui/react';
+import { supabase } from '../../src/libs/supabase-client';
+import type { Product } from '../../src/features/Product';
 
-const ProductIndex: NextPage = () => {
+type Props = {
+  Products: Product[];
+  keyword?: string;
+  type?: string;
+};
+
+const ProductIndex: NextPage<Props> = ({ Products }) => {
   return <VStack spacing={16}></VStack>;
 };
 
 export default ProductIndex;
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const id = context.params?.id as string;
-//   const { data, error, status } = await supabase
-//     .from('products')
-//     .select(
-//       `
-//   *,
-//   brands (
-//     *
-//   )
-// `
-//     )
-//     .eq('id', id)
-//     .single();
-//   if (error && status !== 406) {
-//     throw error;
-//   }
-//   return {
-//     props: {
-//       product: data,
-//     },
-//   };
-// };
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const keyword = context.query.keyword;
+  const type = context.query.type;
+  let query = supabase.from('products').select(
+    `
+      *,
+    brands (
+      *
+    )
+    `
+  );
+  if (keyword) {
+    query = query.like('name', `%${keyword}%`);
+  }
+  if (type) {
+    query = query.eq('type', `${type}`);
+  }
+  const { data, error, status } = await query;
+
+  if (error && status !== 406) {
+    throw error;
+  }
+
+  return {
+    props: {
+      Products: data,
+      keyword: keyword,
+      type: type,
+    },
+  };
+};
